fb95414480e9bc09d72ba7ec4c653a7f36c3e03a
stevenlavelle1 <21316945@studentmail.ul.ie>
2024-11-24 17:04:08 +0000

    typo

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from patterns.command.command_pattern import (
 
 # Main function to run the program
 def main():
-    config_mnager = ConfigurationManager()
+    config_manger = ConfigurationManager()
     # Set up our Command Invoker
     invoker = Invoker()
 
